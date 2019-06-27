a3dbb23b9ceeceecab6088c9361863b35511d281
Vladimir Sartakov <vov.sartakov@mail.ru>
2019-06-28 00:59:52 +0300

    FEATURE: exchenge form style

--- app/components/quick-exchange/QuickExchangeForm.vue
+++ app/components/quick-exchange/QuickExchangeForm.vue
@@ -1,21 +1,25 @@
 <template>
   <form v-if="data.give && data.get" class="quick-exchange-form" @submit.prevent="submit()">
-    <div class="quick-exchange-form__rate">1 {{ data.give | uppercase }} = {{ data.rate }} {{ data.get | uppercase }}</div>
+    <div class="quick-exchange-form__rate"><span>1 {{ data.give | uppercase }}</span> ≈ <span>{{ data.rate }} {{ data.get | uppercase }}</span></div>
     <div class="quick-exchange-form__item">
       <label class="quick-exchange-form__label">You give</label>
-      <div class="quick-exchange-form__input">
-        <input v-model.number="give" type="number" required>
-        <span>{{ data.give | uppercase }}</span>
-      </div>
-      <div class="quick-exchange-form__sep">
-        <svg-icon class="svg-icon quick-exchange-form__sep-icon" :name="'icons/arrow-double-down'" />
-      </div>
-      <div class="quick-exchange-form__input">
-        <input v-model.number="get" type="number" required>
-        <span>{{ data.get | uppercase }}</span>
+      <div class="title-input">
+        <input v-model.number="give" class="title-input__inp" type="text" required>
+        <span class="title-input__title">{{ data.give | uppercase }}</span>
       </div>
     </div>
-    <button class="quick-exchange-form__input" type="submit">Proceed to exchange</button>
+    <div class="quick-exchange-form__sep">
+      <svg-icon class="svg-icon quick-exchange-form__sep-icon" :name="'icons/arrow-double-down'" />
+    </div>
+    <div class="quick-exchange-form__item">
+      <label class="quick-exchange-form__label">You receive</label>
+      <dir class="title-input">
+        <input v-model.number="get" class="title-input__inp" type="text" required>
+        <span class="title-input__title">{{ data.get | uppercase }}</span>
+      </dir>
+    </div>
+    </div>
+    <button class="quick-exchange-form__submit" type="submit">Proceed to exchange</button>
   </form>
 </template>
 
